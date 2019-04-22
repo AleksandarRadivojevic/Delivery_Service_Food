1c688e3d58c9cf67bcdd4a3fec016ea295016022
AleksandarRadivojevic <45386161+AleksandarRadivojevic@users.noreply.github.com>
2019-04-23 00:04:52 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,19 @@ document.getElementById("faultMsg").style.visibility = 'hidden';
 let listProduct = [];
 var finalResult;
 var finalCategories;
+var currentProduct;
+
+// using data about customer
+var currentCustomerString = sessionStorage.getItem('currentObj');
+var currentCustomer = JSON.parse(currentCustomerString);
+
+$('#name').val(currentCustomer.FirstName);
+$('#surname').val(currentCustomer.LastName);
+$('#inputAddress').val(currentCustomer.Address);
+$('#inputCity').val(currentCustomer.City);
+$('#inputState').val(currentCustomer.Country);
+$('#inputZip').val(currentCustomer.PostalCode);
+
 //initial filling in html
 $.ajax({
     url: "https://services.odata.org/V3/Northwind/Northwind.svc/Products?$format=json",
@@ -26,9 +39,10 @@ $.ajax({
                     var product = finalResult[i]
                     listProduct.push(product)
 
+                    currentProduct = finalResult[i];
                     var category = returnCategoryName(finalResult[i], finalCategories);
                     var imgSrc = setPicture(category);
-                    crtElmnt(finalResult[i].ProductName, category, '1', imgSrc, parseInt(finalResult[i].UnitPrice))
+                    crtElmnt(finalResult[i], finalResult[i].ProductName, category, '1', imgSrc, parseInt(finalResult[i].UnitPrice))
                 }
                 // set categories in dropdown list
                 var category = document.getElementById('category');
@@ -85,38 +99,54 @@ function setPicture(parameter) {
 }
 //show basket
 function showBasket() {
+    $('#paymentPage').hide();
+    $('#confirmMsg').hide();
+
+    document.getElementById('listBasket').innerHTML = ''
     for (let i = 0; i < basketArr.length; i++) {
+        var productObject = JSON.parse(basketArr[i])
         var listBasket = document.getElementById('listBasket');
 
-        var product = document.createElement('li');
-        product.innerHTML = basketArr[i];
-        listBasket.appendChild(product);
+        var row = document.createElement('tr');
+        listBasket.appendChild(row)
+
+        var product = document.createElement('td');
+        product.innerHTML = productObject.ProductName;
+        row.appendChild(product);
+
+        var quantity = document.createElement('td');
+        quantity.innerHTML = 'Quantity: 1';
+        row.appendChild(quantity);
+
+        var price = document.createElement('td');
+        price.innerHTML = 'Price: ' + productObject.UnitPrice + ' $';
+        row.appendChild(price);
+
         console.log(basketArr)
     }
 }
 // addition 
-function add(id) {
+function add(id, val) {
     document.getElementById("faultMsg").style.visibility = 'hidden';
     let priceArticle = parseInt(document.getElementById('price' + id).innerHTML);
     let quantityArticle = parseInt(document.getElementById('quantity' + id).value);
     totalSum = totalSum + priceArticle * quantityArticle;
     $('#result').text(totalSum);
-
-    // prepraviti ne valja dodavanje u korpu tj debaguj sto ubacuje sve artikle
+    $('#resultModal').text(totalSum);
 
     for (let i = 0; i < quantityArticle; i++) {
-        basketArr.push(id);
+        basketArr.push(val);
     }
 }
 // subtract
-function minus(id) {
+function minus(id, val) {
     let priceArticle = parseInt(document.getElementById('price' + id).innerHTML);
     let quantityArticle = parseInt(document.getElementById('quantity' + id).value);
     let basketElement = 0;
 
     // check how much we have elements in basket
     for (let i in basketArr) {
-        if (basketArr[i] == id) {
+        if (basketArr[i] == val) {
             basketElement = basketElement + 1;
         }
     }
@@ -127,11 +157,12 @@ function minus(id) {
         document.getElementById("quantity" + id).value = 1;
     } else {
         for (let i = 0; i < quantityArticle; i++) {
-            let element = basketArr.indexOf(id);
+            let element = basketArr.indexOf(val);
             basketArr.splice(element, 1);
         }
         totalSum = totalSum - priceArticle * quantityArticle;
         $('#result').text(totalSum);
+        $('#resultModal').text(totalSum);
         document.getElementById("faultMsg").style.visibility = 'hidden';
     }
 }
@@ -146,11 +177,11 @@ function filterArticles() {
         var imgSrc = setPicture(category);
         if (inputCategory == 'null') {
             if (nameProductFilter.toLowerCase().indexOf(inputFilter) > -1) {
-                crtElmnt(listProduct[i].ProductName, category, '1', imgSrc, parseInt(listProduct[i].UnitPrice))
+                crtElmnt(listProduct[i], listProduct[i].ProductName, category, '1', imgSrc, parseInt(listProduct[i].UnitPrice))
             }
         } else {
             if (nameProductFilter.toLowerCase().indexOf(inputFilter) > -1 && listProduct[i].CategoryID == inputCategory) {
-                crtElmnt(listProduct[i].ProductName, category, '1', imgSrc, parseInt(listProduct[i].UnitPrice))
+                crtElmnt(listProduct[i], listProduct[i].ProductName, category, '1', imgSrc, parseInt(listProduct[i].UnitPrice))
             }
         }
     }
@@ -165,7 +196,7 @@ function closeModal(id, style) {
     document.getElementById(id).style = style;
 }
 // create new article
-function crtElmnt(nameArticle, categoryArticle, quantityArticle, srcImgElement, priceElement) {
+function crtElmnt(objectAPI, nameArticle, categoryArticle, quantityArticle, srcImgElement, priceElement) {
 
     var wrapp = document.getElementById("container");
     // card
@@ -218,24 +249,51 @@ function crtElmnt(nameArticle, categoryArticle, quantityArticle, srcImgElement, 
     // button add in basket
     var btnAdd = document.createElement('button');
     btnAdd.innerHTML = "Add";
+    btnAdd.setAttribute("value", JSON.stringify(objectAPI));
     btnAdd.setAttribute("id", "P" + numNewArticle);
     btnAdd.setAttribute("class", "btn btn-success");
-    btnAdd.setAttribute("onclick", "add(this.id)");
+    btnAdd.setAttribute("onclick", "add(this.id, event.target.value)");
     btnAdd.style.marginRight = "5px";
     elementBox.appendChild(btnAdd);
 
     // button subtract from basket
     var btnSubtract = document.createElement('button');
     btnSubtract.innerHTML = "Subtract";
+    btnSubtract.setAttribute("value", JSON.stringify(objectAPI));
     btnSubtract.setAttribute("id", "P" + numNewArticle);
     btnSubtract.setAttribute("class", "btn btn-danger");
-    btnSubtract.setAttribute("onclick", "minus(this.id)");
+    btnSubtract.setAttribute("onclick", "minus(this.id, event.target.value)");
     elementBox.appendChild(btnSubtract);
 
     numNewArticle = numNewArticle + 1;
 }
-// 15. drugi deo oko filtriranja kako?
-// 16. postaviti datepicker jquery ui
-// 17. input file jqueri
-// problem categories u add article
+// payment in modal
+function payment() {
+    $('#listForBuy').hide();
+    $('#paymentPage').show()
+    $('#confirmMsg').hide();
+
+}
+// confirm message in modal
+var validationSwitch = true;
+
+function confirm() {
+    var listID = ['name', 'surname', 'inputAddress', 'inputCity', 'inputState', 'inputZip', 'cardNum', 'exp', 'ccv'];
+
+    for (let i = 0; i < listID.length; i++) {
+        var element = document.getElementById(listID[i]).value;
+        if (!element) {
+            document.getElementById(listID[i]).style.border = '1px solid red'
+            return;
+        } else {
+            document.getElementById(listID[i]).style.border = '1px solid #ccc'
+        }
+    }
+
+    $('#listForBuy').hide();
+    $('#paymentPage').hide()
+    $('#confirmMsg').show()
+
+}
+
 
