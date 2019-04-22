2a8dae1342dfb7c9620286bee0a37d4680b01150
AleksandarRadivojevic <45386161+AleksandarRadivojevic@users.noreply.github.com>
2019-04-23 00:05:24 +0200

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
     $("#btnLogin").click(function () {
+        $("#btnLogin").hide();
+        $("#btnDisabled").show();
         $.ajax({
             url: 'https://services.odata.org/V3/Northwind/Northwind.svc/Employees?%24format=json',
             dataType: "json",
             success: function (result) {
+                $("#btnLogin").show();
+                $("#btnDisabled").hide();
+
                 console.log(result);
                 let finalResult = result.value;
                 let username = document.getElementById('username').value;
@@ -12,6 +17,11 @@ $(document).ready(function () {
                 for (let i = 0; i < finalResult.length; i++) {
                     if (username == finalResult[i].FirstName) {
                         if (password == finalResult[i].LastName) {
+
+                            // set object in storage
+                            var objString = JSON.stringify(finalResult[i])
+                            sessionStorage.setItem("currentObj", objString);
+
                             window.location.assign('index.html')
                         } else {
                             document.getElementById('username').style.border = '1px solid #ccc';
